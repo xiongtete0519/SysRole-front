a37c104c30e9ca74e8498e556ccce046afb7fefe
CodeLeader <1144722582@qq.com>
2022-12-03 00:24:00 +0800

    菜单管理

--- src/router/index.js
+++ src/router/index.js
@@ -73,6 +73,24 @@ export const constantRoutes = [
         name: 'sysUser',
         component: () => import('@/views/system/sysUser/list'),
         meta: { title: '用户管理', icon: 'tree' }
+      },
+      {
+        name: 'sysMenu',
+        path: 'sysMenu',
+        component: () => import('@/views/system/sysMenu/list'),
+        meta: {
+          title: '菜单管理',
+          icon: 'el-icon-s-unfold'
+        },
+      },
+      {
+        path: 'assignAuth',
+        component: () => import('@/views/system/sysRole/assignAuth'),
+        meta: {
+          activeMenu: '/system/sysRole',
+          title: '角色授权'
+        },
+        hidden: true,
       }
     ]
   },
